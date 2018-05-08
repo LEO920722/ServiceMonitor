fff2d9fc0fe7fb07c65ff5d99a97ff812aef2ff6
Leo Wei <Leo Wei@DESKTOP-DRK2Q3H>
2018-05-08 11:53:37 +0800

    Update Initial version with useless files

--- src/com/yisquare/db/DBConf.java
+++ src/com/yisquare/db/DBConf.java
@@ -11,9 +11,9 @@ import com.yisquare.tools.PropertiesUtil;
 public class DBConf {  
 	private static Logger logger = Logger.getLogger(DBConf.class);
     private static final String name = "oracle.jdbc.driver.OracleDriver";  
-	private static String url = "jdbc:oracle:thin:@10.143.181.60:1521:cnywmdev";  //当部署的时候请将ip改为相应的ip
-    private static String user = "CNINTWMDVCA";  
-    private static String password = "wmdvca1607";  
+//	private static String url = "jdbc:oracle:thin:@10.143.181.60:1521:cnywmdev";  //当部署的时候请将ip改为相应的ip
+//    private static String user = "CNINTWMDVCA";  
+//    private static String password = "wmdvca1607";  
     private  Connection conn=null;
    
     public  Connection getConn() {
@@ -21,8 +21,8 @@ public class DBConf {
 	        Class.forName(name); 
 	        String ipString = PropertiesUtil.getDBInfo();//获取相关的db信息
 	        String ipArray [] = ipString.split("-");
-//	        conn = DriverManager.getConnection("jdbc:oracle:thin:@"+ipArray[0]+":"+ipArray[1]+":"+ipArray[2], ipArray[3], ipArray[4]);  
-	        conn = DriverManager.getConnection(url,user,password);
+	        conn = DriverManager.getConnection("jdbc:oracle:thin:@"+ipArray[0]+":"+ipArray[1]+":"+ipArray[2], ipArray[3], ipArray[4]);  
+//	        conn = DriverManager.getConnection(url,user,password);
 	    } catch (Exception e) {  
 	    	logger.error("Exception:"+LogCreate.getException(e));
 	        e.printStackTrace();  
